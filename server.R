78d3e769fa948c141ad002d328dea1215348b5ae
db-bangs <98397769+db-bangs@users.noreply.github.com>
2023-04-03 08:53:18 -0400

    Add moving rolling average and KPIs
    Add Service Standard slider
    Add Rolling Average geom_line
    Add KPI valueBoxes

--- server.R
+++ server.R
@@ -2,90 +2,169 @@
 
 ## Initialize ####
 library(shiny)
+library(shinydashboard)
+library(dplyr)
 library(simmer)
 library(MASS)
+library(zoo)
 library(ggplot2)
+
+# Set Random Seed ####
+set.seed(42)
+
+# Suppress Warnings and Messages
+options(warn=-1)
+options(verbose = FALSE)
 
 shinyServer(function(input, output) {
 
+  # Queue Simulator ####
+  ## Call Data Simulator####
+  call.data <- reactive({
+    
+    ### Inputs ####
+    service_rate <- input$service
+    arrival_rate <- input$arrival / 60
+    agents <- input$agents
+    standard <- input$standard
+    
+    ### Simulation ####
+    customer <-
+      trajectory("Customer's path") %>%
+      seize("counter") %>%
+      timeout(function() {rexp(1, 1/service_rate)}) %>%
+      release("counter")
+    
+    bank <-
+      simmer("bank") %>%
+      add_resource("counter", agents) %>%
+      add_generator("Customer", customer, function() {c(0,rexp(1000, arrival_rate), -1)})
+    
+    bank %>% run(until = 600)
+    
+    result <-
+      bank %>%
+      get_mon_arrivals() %>%
+      transform(waiting_time = round(end_time - start_time - activity_time, 2))
+    
+    result <- result %>%
+      mutate(rollmean = rollmean(waiting_time, 11, fill = NA, align = "center"))
+    
+  }) %>%
+    bindEvent(input$sim,
+              input$service,
+              input$arrival,
+              input$agents,
+              ignoreNULL = TRUE,
+              ignoreInit = FALSE)
+  
+  
+  
+  
+  ## Plotting ####
+  observeEvent(input$sim, ignoreNULL = FALSE, ignoreInit = FALSE, {
+    
     output$simmerPlot <- renderPlot({
       
-      service_rate <- input$service
-      arrival_rate <- input$arrival / 60
-      agents <- input$agents
+      if(nrow(call.data()) >= 11 ){
+        call.data() %>%
+          ggplot(aes(start_time, waiting_time)) +
+          geom_hline(aes(yintercept = input$standard / 60, color = "Service Standard"), size = 2) +
+          geom_line(data = call.data(), aes(x = start_time, y=rollmean, color = "Rolling Average"),
+                    size = 2, show.legend = TRUE) +
+          scale_color_manual("Legend", values = c("Rolling Average" = "firebrick",
+                                                  "Service Standard" = "gray")) +
+          geom_point() +
+          labs(x = "Start Time (Minutes)",
+               y = "Time in Queue (Minutes)",
+               legend = "") +
+          xlim(c(0,600)) +
+          theme_classic() +
+          theme(axis.title = element_text(size = 16, face = "bold"),
+                plot.title = element_text(size = 16, face = "bold"),
+                axis.text = element_text(size = 12, face = "bold"),
+                legend.title = element_blank(),
+                legend.text = element_text(size = 14, face = "bold"),
+                legend.position = c(.10, .95))
+      } else {
+        call.data() %>%
+          ggplot(aes(start_time, waiting_time)) +
+          geom_hline(aes(yintercept = input$standard / 60, color = "Service Standard"), size = 2) +
+          scale_color_manual("Legend", values = c("Rolling Average" = "firebrick",
+                                                  "Service Standard" = "gray")) +
+          geom_point() +
+          labs(x = "Start Time (Minutes)",
+               y = "Time in Queue (Minutes)",
+               legend = "") +
+          xlim(c(0,600)) +
+          theme_classic() +
+          theme(axis.title = element_text(size = 16, face = "bold"),
+                plot.title = element_text(size = 16, face = "bold"),
+                axis.text = element_text(size = 12, face = "bold"),
+                legend.title = element_blank(),
+                legend.text = element_text(size = 14, face = "bold"),
+                legend.position = c(.10, .95))
+      }
       
-      customer <-
-        trajectory("Customer's path") %>%
-        seize("counter") %>%
-        timeout(function() {rexp(1, 1/service_rate)}) %>%
-        release("counter")
-      
-      bank <-
-        simmer("bank") %>%
-        add_resource("counter", agents) %>%
-        add_generator("Customer", customer, function() {c(0,rexp(1000, arrival_rate), -1)})
-      
-      bank %>% run(until = 600)
-      
-      result <-
-        bank %>%
-        get_mon_arrivals() %>%
-        transform(waiting_time = round(end_time - start_time - activity_time, 2))
-      
-      result %>%
-        ggplot(aes(start_time, waiting_time)) +
-        geom_point() +
-        labs(title = paste("Avg Wait: ", round(mean(result$waiting_time),2), " minutes", " | ",
-                           "Max Wait: ", round(max(result$waiting_time),2), " minutes", sep = ""),
-             x = "Start Time (Minutes)",
-             y = "Time in Queue") +
-        xlim(c(0,600)) +
-        theme_classic() +
-        theme(axis.title = element_text(size = 16, face = "bold"),
-              plot.title = element_text(size = 16, face = "bold"),
-              axis.text = element_text(size = 12, face = "bold"))
-      
-  })
-    
-  observeEvent(input$sim, {
-    
-    output$simmerPlot <- renderPlot({
-      
-      service_rate <- input$service
-      arrival_rate <- input$arrival / 60
-      agents <- input$agents
-      
-      customer <-
-        trajectory("Customer's path") %>%
-        seize("counter") %>%
-        timeout(function() {rexp(1, 1/service_rate)}) %>%
-        release("counter")
-      
-      bank <-
-        simmer("bank") %>%
-        add_resource("counter", agents) %>%
-        add_generator("Customer", customer, function() {c(0,rexp(1000, arrival_rate), -1)})
-      
-      bank %>% run(until = 600)
-      
-      result <-
-        bank %>%
-        get_mon_arrivals() %>%
-        transform(waiting_time = round(end_time - start_time - activity_time, 2))
-      
-      result %>%
-        ggplot(aes(start_time, waiting_time)) +
-        geom_point() +
-        labs(title = paste("Avg Wait: ", round(mean(result$waiting_time),2), " minutes", " | ",
-                           "Max Wait: ", round(max(result$waiting_time),2), " minutes", sep = ""),
-             x = "Start Time (Minutes)",
-             y = "Time in Queue") +
-        xlim(c(0,600)) +
-        theme_classic() +
-        theme(axis.title = element_text(size = 16, face = "bold"),
-              plot.title = element_text(size = 16, face = "bold"),
-              axis.text = element_text(size = 12, face = "bold"))
       
     })
   })
+  
+  
+  
+  ## Performance Table ####
+  output$simmerTable <- renderDataTable({
+    call.data() %>%
+      mutate(meets_standard = waiting_time <= (input$standard / 60)) %>%
+      summarize(total_calls = n(),
+                met_standard = paste(round((sum(meets_standard / n() * 100)), 2), "%"),
+                avg_wait = paste(round(mean(waiting_time*60), 0), "sec"),
+                max_wait = paste(round(max(waiting_time*60), 0), "sec")
+      ) %>%
+      datatable(
+        rownames = FALSE,
+        colnames = c("Total Calls" = "total_calls",
+                     "Avg. Wait" = "avg_wait",
+                     "Max. Wait" = "max_wait",
+                     "Met Standard" = "met_standard"),
+        options = list(
+          'dom' = 't',
+          columnDefs = list(list(className = 'dt-center', targets = 0:3)),
+          ordering = FALSE)
+      )
+  })
+  
+  
+  ## KPI valueBoxes ####
+  output$totalCalls_box <- renderValueBox(
+    call.data() %>%
+      summarize(total_calls = n()) %>%
+      valueBox("Total Calls")
+  )
+  
+  output$metStandard_box <- renderValueBox(
+    call.data() %>%
+      mutate(meets_standard = waiting_time <= (input$standard / 60)) %>%
+      summarize(met_standard = paste(round((sum(meets_standard / n() * 100)), 1), "%")) %>%
+      valueBox("Met Standard")
+  )
+  
+  
+  output$percentile80_box <- renderValueBox(
+    call.data() %>%
+      summarize(percentile80 = paste(round(quantile(waiting_time * 60, probs = 0.8), 0), "sec")) %>%
+      valueBox("80th Percentile")
+  )
+  
+  output$avgCall_box <- renderValueBox(
+    call.data() %>%
+      summarize(avg_wait = paste(round(mean(waiting_time*60), 0), "sec")) %>%
+      valueBox("Average Wait")
+  )
+  
+  output$maxCall_box <- renderValueBox(
+    call.data() %>%
+      summarize(max_wait = paste(round(max(waiting_time*60), 0), "sec")) %>%
+      valueBox("Maximum Wait")
+  )
 })
--- ui.R
+++ ui.R
@@ -2,9 +2,18 @@
 
 ## Initialize ####
 library(shiny)
+library(shinydashboard)
+library(dplyr)
 library(simmer)
 library(MASS)
 library(ggplot2)
+
+# Set Random Seed ####
+set.seed(42)
+
+# Suppress Warnings and Messages
+options(warn=-1)
+options(verbose = FALSE)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -12,88 +21,109 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("Call Centre Queue: Discrete-Event Simulator"),
 
-    # Sidebar with a slider input for number of bins
     sidebarLayout(
-        sidebarPanel(
-          
-          p("This simulator generates calls and queue times for one business day
-            using the three variables you set below. 
+      sidebarPanel(
+        
+        ### Introduction Text ####
+        p("This simulator generates calls and queue times for one business day.
             Up to 1000 calls over 10 hours enter the queue and are
             served as soon as the next agent becomes available, or immediately if
-            the queue is empty. Press 'Simulate Again' to see how the same scenario 
-            can play out differently day-to-day."),
-          
-          p("The simulation will stop taking calls
-            when the last will only be served at the end of the day. Try to
-            'break' the system and see what it takes to overwhelm your call
-            centre."),
-          
-          
-          br(),
-          
-            sliderInput("agents",
-                        "Number of Agents:",
-                        min = 1,
-                        max = 10,
-                        value = 3),
-            
-            sliderInput("arrival",
-                        "Arrival Rate (calls per hour):",
-                        min = 1,
-                        max = 60,
-                        value = 30),
-            
-            sliderInput("service",
-                        "Average Service Time (minutes):",
-                        min = 1,
-                        max = 10,
-                        value = 5),
-            actionButton("sim", "Simulate Again"),
-          
-          
-          p(" "),
-          
-          br(),
-          
-          tags$div(
-            "To see a complete year's time series of calls, download the dataset on ",
-            tags$a(href="https://www.kaggle.com/datasets/donovanbangs/call-centre-queue-simulation",
-                   "Kaggle.")
-          ),
-          
-          br(),
-          
-          tags$div(
-            "View the source code on ",
-            tags$a(href = "https://github.com/db-bangs/Queue-Simmer",
-                   "GitHub.")
-          ),
-          
-          br(),
-          
-          tags$div(
+            the queue is empty."),
+        
+        p("Use the sliders below to set the service standard and parameters
+                                for your call centre. Simulate again and again to see how a scenario
+                                will play out differently each day."),
+        
+        ### Controls ####
+        #### Service Standard Slider ####
+        sliderInput("standard",
+                    "Service Standard (Seconds):",
+                    min = 0,
+                    max = 240,
+                    value = 120,
+                    step = 10),
+        
+        #### Number of Agents Slider ####
+        sliderInput("agents",
+                    "Number of Agents:",
+                    min = 1,
+                    max = 10,
+                    value = 3),
+        
+        #### Arrival Rate Slider ####
+        sliderInput("arrival",
+                    "Arrival Rate (calls per hour):",
+                    min = 0,
+                    max = 60,
+                    value = 30),
+        
+        #### Service Time Slider ####
+        sliderInput("service",
+                    "Average Service Time (minutes):",
+                    min = 1,
+                    max = 10,
+                    value = 5),
+        
+        #### Simulation Action ####
+        actionButton("sim", "Simulate Again"),
+        
+        
+        p(" "),
+        
+        br(),
+        
+        tags$div(
+          "To see a complete year's time series of calls, download the dataset on ",
+          tags$a(href="https://www.kaggle.com/datasets/donovanbangs/call-centre-queue-simulation",
+                 "Kaggle.")
+        ),
+        
+        br(),
+        
+        tags$div(
+          "View the source code on ",
+          tags$a(href = "https://github.com/db-bangs/Queue-Simmer",
+                 "GitHub.")
+        ),
+        
+        br(),
+        
+        tags$div(
           "The simulation is created with package
             `simmer` (Ucar et al., 2019). Refer to their ",
           tags$a(href="https://r-simmer.org/",
-          "website "),
+                 "website "),
           "for details on how the discrete-event simulations are performed."),
-          
-          br(),
-          
-          p("Ucar I, Smeets B, Azcorra A (2019). “simmer: Discrete-Event Simulation for R.” Journal of Statistical Software, 90(2), 1–30."),
-          
-          br(),
-          
-          tags$div(
-            "Shiny App by ",
-            tags$a(href = "https://github.com/db-bangs",
-            "Donovan Bangs "),
-            "- last updated September 19, 2022")
-          ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("simmerPlot", height = 600)
-        )
+        
+        br(),
+        
+        p("Ucar I, Smeets B, Azcorra A (2019). “simmer: Discrete-Event Simulation for R.” Journal of Statistical Software, 90(2), 1–30."),
+        
+        br(),
+        
+        tags$div(
+          "Shiny App by ",
+          tags$a(href = "https://github.com/db-bangs",
+                 "Donovan Bangs "),
+          "- last updated January 29, 2023")
+      ),
+      
+      ### Main Panel ####
+      mainPanel(
+        #### KPI Boxes ####
+        fluidRow(
+          valueBox("", subtitle = "", width = 1),
+          valueBoxOutput("totalCalls_box", width = 2),
+          valueBoxOutput("metStandard_box", width = 2),
+          valueBoxOutput("avgCall_box", width = 2),
+          valueBoxOutput("percentile80_box", width = 2),
+          valueBoxOutput("maxCall_box", width = 2),
+          align = 'center'
+        ),
+        #### KPI Table ####
+        #dataTableOutput("simmerTable"),
+        #### Plot ####
+        plotOutput("simmerPlot", height = 600)
+      )
     )
 ))
